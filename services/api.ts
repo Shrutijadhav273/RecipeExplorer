06beba4d12a1eb25a0e05408a237a2b46573cf5e
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-05-24 17:03:27 +0530

    api key updated

--- services/api.ts
+++ services/api.ts
@@ -6,7 +6,7 @@ const API = axios.create({
   headers: {
     'Content-Type': 'application/json',
 
-    'x-api-key': 'reqres-free-v1',
+    'x-api-key': 'pro_4dcb5bdbdd1f5a2014098b1c47f6c01bbf76e9c653d3cb6fe25bca0c38a2ad10',
   },
 });
 
